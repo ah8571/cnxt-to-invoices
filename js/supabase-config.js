985db9f296229e0c636e172de87f378c16d9493f
ah8571 <ah8571@github.com>
2026-05-09 21:43:24 -0400

    Add Supabase project URL and anon key

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 export const supabaseConfig = {
-  url: "",
-  anonKey: "",
+  url: "https://jstojewashwoswsskwjk.supabase.co/rest/v1/",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzdG9qZXdhc2h3b3N3c3Nrd2prIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNTg2OTAsImV4cCI6MjA5MzkzNDY5MH0.o3hYxYr1ZbmEShPfZebx1vchjmIrN7uYZMX1C5fhoac",
 };
